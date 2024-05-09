cd6ec2f6e569726780be7a53525608ed326a5d06
Nickjs101 <Nickjoshuamontemayor0121@gmail.com>
2024-05-09 15:36:33 +0800

    Done Tutorial

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,14 +1,22 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
+import About from "./components/About";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
-    <div className="App bg-black">
+    <div className="App">
       <Navbar/>
       <Hero/>
       <Skills/>
+      <About/>
+      <Work/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ my-portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='max-x-[1200px] sm:h-[150px] p-12 flex justify-between mx-auto'>
+        <span className='primary-color'>NICK JOSHUA POSADAS MONTEMAYOR</span>
+        <p className='text-gray-600'>nickjoshuamontemayor0121@gmail.com <br/> #IT2024</p>
+    </div>
+  )
+}
+
+export default Footer
